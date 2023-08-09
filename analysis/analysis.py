91b3668134f84548bd2e74c5293b82d2a316b592
mh-lut <82096888+mh-lut@users.noreply.github.com>
2023-08-09 18:36:54 +0300

    Update analysis.py

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -66,91 +66,6 @@ def check_pairs(database1, database2):
     
     return matching_pairs_list, unmatched_rows_list
 
-#Count how many messages of that size are in the list
-def calculate_length(rows, x):
-    amount = 0
-    for row in rows:
-        message, time_start, time_finished, time_difference, answer, lat, lon, speed = row
-        if len(message) == x:
-            amount += 1
-    return amount
-
-# Draw lost message graph %
-def lost_message_graph(matching_pairs_list, unmatched_rows_list):
-    message_sizes = [1, 4, 8, 12]  # List of message sizes
-    total_messages = len(matching_pairs_list) + len(unmatched_rows_list)  # Total number of messages
-    print("Total messages: " + str(total_messages))
-    print("Total pairs: " + str(len(matching_pairs_list)))
-    print("Total unmatchs: " + str(len(unmatched_rows_list)))
-    
-    unmatched_1 = []
-    unmatched_4 = []
-    unmatched_8 = []
-    unmatched_12 = []
-    
-    # Separate the data into four lists based on the message lenght
-    for message, time_start, time_finished, time_difference, answer, lat, lon, speed in unmatched_rows_list:
-        if len(message)/2 == 1:
-            unmatched_1.append([message, time_start, time_finished, time_difference, answer, lat, lon, speed])
-        elif len(message)/2 == 4:
-            unmatched_4.append([message, time_start, time_finished, time_difference, answer, lat, lon, speed])
-        elif len(message)/2 == 8:
-            unmatched_8.append([message, time_start, time_finished, time_difference, answer, lat, lon, speed])
-        elif len(message)/2 == 12:
-            unmatched_12.append([message, time_start, time_finished, time_difference, answer, lat, lon, speed])
-    unmatched_counts = [unmatched_1, unmatched_4, unmatched_8, unmatched_12]  # List of unmatched message counts for each size
-
-    # Calculate the percentage of unmatched messages for each size
-    percent_unmatched = [(len(count) / total_messages) * 100 for count in unmatched_counts]
-
-    # Create a bar plot
-    plt.figure(figsize=(10, 6))  # Figure size
-    bars = plt.bar(message_sizes, percent_unmatched, color='red') # Bars
-    
-    # Add labels, title, and legend...
-    plt.xlabel('Message Size')
-    plt.ylabel('Percentage of Unmatched Messages')
-    plt.title('Percentage of Unmatched Messages by Message Size')
-    plt.xticks(message_sizes) # X-axis
-    plt.yticks(range(0, 10, 1))  # Y-axis
-    plt.grid(axis='y', linestyle='-', alpha=0.8) # Add grid - only across
-
-    # Add labelt top of the bars
-    add_value_labels(bars, plt)
-
-    # Display the graph
-    plt.tight_layout()
-    plt.show()
-    return None
-
-# Accumulation of delays graph
-def delay_graph(delay_lists):
-
-    delay_only_list_1 = [innerlist[0] for innerlist in delay_lists[0]]
-    delay_only_list_4 = [innerlist[0] for innerlist in delay_lists[1]]
-    delay_only_list_8 = [innerlist[0] for innerlist in delay_lists[2]]
-    delay_only_list_12 = [innerlist[0] for innerlist in delay_lists[3]]
-    
-    # Create a cumulative distribution plot for each dataset
-    plt.hist(delay_only_list_1, bins=1000, density=True, histtype='step', color='blue', cumulative=True, alpha=0.5, label='Message lenght 1')
-    plt.hist(delay_only_list_4, bins=1000, density=True, histtype='step', color='green', cumulative=True, alpha=0.5, label='Message lenght 4')
-    plt.hist(delay_only_list_8, bins=1000, density=True, histtype='step', color='red',cumulative=True, alpha=0.5, label='Message lenght 8')
-    plt.hist(delay_only_list_12, bins=1000, density=True, histtype='step', color='black',cumulative=True, alpha=0.5, label='Message lenght 12')
-
-    # Add labels and title
-    plt.xlabel('Time')
-    plt.ylabel('Cumulative Probability')
-    plt.title('Cumulative Distribution Plot')
-
-    # Add a legend
-    plt.legend()
-
-    # Show the plot
-    plt.grid()
-    plt.show()
-
-    return None
-
 # Count the delays per message size
 def calculate_delay(matching_pairs_list):
     delay_list_1 = []
@@ -190,26 +105,166 @@ def calculate_delay(matching_pairs_list):
     
     return delay_lists
 
+# For lost_message_graph
+def calculate_percentage(unmatched_range, total_range, lower_bound, upper_bound):
+    matched_range = [num for num in total_range if lower_bound <= float(num) <= upper_bound]
+    if not matched_range:
+        return 0
+    return (len([num for num in unmatched_range if lower_bound <= float(num) <= upper_bound]) / 
+            len(matched_range)) * 100
+
+# Draw lost message graph %
+def lost_message_graph(matching_pairs_list, unmatched_rows_list):
+ 
+    unmatched_1 = []
+    unmatched_4 = []
+    unmatched_8 = []
+    unmatched_12 = []
+
+    matched_1 = []
+    matched_4 = []
+    matched_8 = []
+    matched_12 = []
+    
+    # Separate the data into four lists based on the message lenght
+    for message, _, _, _, _, _, _, speed in unmatched_rows_list:
+        if len(message)/2 == 1:
+            unmatched_1.append(speed)
+        elif len(message)/2 == 4:
+            unmatched_4.append(speed)
+        elif len(message)/2 == 8:
+            unmatched_8.append(speed)
+        elif len(message)/2 == 12:
+            unmatched_12.append(speed)
+    
+    for pair in matching_pairs_list:
+        message, _, _, _, _, _, _, speed = pair[0]
+        if len(message)/2 == 1:
+            matched_1.append(speed)
+        elif len(message)/2 == 4:
+            matched_4.append(speed)
+        elif len(message)/2 == 8:
+            matched_8.append(speed)
+        elif len(message)/2 == 12:
+            matched_12.append(speed)
+
+    total_messages_1 = unmatched_1 + matched_1  # Total number of messages message lenght = 1
+    total_messages_4 = unmatched_4 + matched_4  # Total number of messages message lenght = 4
+    total_messages_8 = unmatched_8 + matched_8  # Total number of messages message lenght = 8
+    total_messages_12 = unmatched_12 + matched_12  # Total number of messages message lenght = 12
+
+    
+    # Calculate the percentage of unmatched messages for each size
+    ranges = [(i, i+10) for i in range(0, 91, 10)]  # Generate ranges from 0 to 100 with step 10
+
+    percent_unmatched_1 = [calculate_percentage(unmatched_1, total_messages_1, lower, upper) for lower, upper in ranges]
+    percent_unmatched_4 = [calculate_percentage(unmatched_4, total_messages_4, lower, upper) for lower, upper in ranges]
+    percent_unmatched_8 = [calculate_percentage(unmatched_8, total_messages_8, lower, upper) for lower, upper in ranges]
+    percent_unmatched_12 = [calculate_percentage(unmatched_12, total_messages_12, lower, upper) for lower, upper in ranges]
+    
+    range_labels = [f'{lower}-{upper}' for lower, upper in ranges]
+    
+    # Create a plot
+    x = numpy.arange(len(range_labels))  # x-coordinates for the bars
+    width = 0.2  # Width of the bars
+
+    fig, ax = plt.subplots()
+    bar_1 = ax.bar(x - 1.5*width, percent_unmatched_1, width, label='Message lenght 1')
+    bar_4 = ax.bar(x - 0.5*width, percent_unmatched_4, width, label='Message lenght 4')
+    bar_8 = ax.bar(x + 0.5*width, percent_unmatched_8, width, label='Message lenght 8')
+    bar_12 = ax.bar(x + 1.5*width, percent_unmatched_12, width, label='Message lenght 12')
+
+    ax.set_xlabel('Speed')
+    ax.set_ylabel('Percentage Unmatched')
+    ax.set_title('Percentage of Unmatched Messages')
+    ax.set_xticks(x)
+    ax.set_xticklabels(range_labels)
+    ax.legend()
+    
+    # Add labels
+    add_value_labels(bar_1, ax)
+    add_value_labels(bar_4, ax)
+    add_value_labels(bar_8, ax)
+    add_value_labels(bar_12, ax)
+
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+
+    plt.show()
+
+    return None
+
+#Count how many messages of that size are in the list
+def calculate_length(rows, x):
+    amount = 0
+    for row in rows:
+        message, time_start, time_finished, time_difference, answer, lat, lon, speed = row
+        if len(message) == x:
+            amount += 1
+    return amount
+
+# Accumulation of delays graph
+def delay_graph(delay_lists):
+
+    delay_only_list_1 = [innerlist[0] for innerlist in delay_lists[0]]
+    delay_only_list_4 = [innerlist[0] for innerlist in delay_lists[1]]
+    delay_only_list_8 = [innerlist[0] for innerlist in delay_lists[2]]
+    delay_only_list_12 = [innerlist[0] for innerlist in delay_lists[3]]
+    
+    # Create a cumulative distribution plot for each dataset
+    plt.hist(delay_only_list_1, bins=1000, density=True, histtype='step', color='blue', cumulative=True, alpha=0.5, label='Message lenght 1')
+    plt.hist(delay_only_list_4, bins=1000, density=True, histtype='step', color='green', cumulative=True, alpha=0.5, label='Message lenght 4')
+    plt.hist(delay_only_list_8, bins=1000, density=True, histtype='step', color='red',cumulative=True, alpha=0.5, label='Message lenght 8')
+    plt.hist(delay_only_list_12, bins=1000, density=True, histtype='step', color='black',cumulative=True, alpha=0.5, label='Message lenght 12')
+
+    # Add labels and title
+    plt.xlabel('Time')
+    plt.ylabel('Cumulative Probability')
+    plt.title('Cumulative Distribution Plot')
+
+    # Add a legend
+    plt.legend()
+
+    # Show the plot
+    plt.grid()
+    plt.show()
+
+    return None
+
 # Function to add value labels on top of the bars
 def add_value_labels(bars, ax):
     for bar in bars:
         yval = bar.get_height() # Bar height
-        ax.annotate(f'{yval:.2f}',
-                    xy=(bar.get_x() + bar.get_width() / 2, yval),
-                    xytext=(0, 0),  # text 
-                    textcoords='offset points',
-                    ha='center', va='bottom')
+        if yval != 0:    
+            ax.annotate(f'{yval:.2f}',
+                        xy=(bar.get_x() + bar.get_width() / 2, yval),
+                        xytext=(0, 0),  # text 
+                        textcoords='offset points',
+                        ha='center', va='bottom')
 
+# Delay graph 2 (min/average/max)
 def delay_graph_2(delay_lists):
     # Calculate min, average, and max for each list
-    derlay_only_list_1 = [innerlist[0] for innerlist in delay_lists[0]]
-    derlay_only_list_4 = [innerlist[0] for innerlist in delay_lists[1]]
-    derlay_only_list_8 = [innerlist[0] for innerlist in delay_lists[2]]
-    derlay_only_list_12 = [innerlist[0] for innerlist in delay_lists[3]]
+    derlay_only_list_1 = []
+    for sublist in delay_lists[0]:
+        derlay_only_list_1.append(sublist[0])
+        
+    derlay_only_list_4 = []
+    for sublist in delay_lists[1]:
+        derlay_only_list_4.append(sublist[0])
+        
+    derlay_only_list_8 = []
+    for sublist in delay_lists[2]:
+        derlay_only_list_8.append(sublist[0])
+    derlay_only_list_12 = []
     
-    min_values = [min(derlay_only_list_1), min(derlay_only_list_4), min(derlay_only_list_8), min(derlay_only_list_12)]
+    for sublist in delay_lists[3]:
+        derlay_only_list_12.append(sublist[0])
+    
+
+    min_values = [min(sublist) if sublist else float('inf') for sublist in [derlay_only_list_1, derlay_only_list_4, derlay_only_list_8, derlay_only_list_12]]
     average_values = [numpy.mean(derlay_only_list_1), numpy.mean(derlay_only_list_4), numpy.mean(derlay_only_list_8), numpy.mean(derlay_only_list_12)]
-    max_values = [max(derlay_only_list_1), max(derlay_only_list_4), max(derlay_only_list_8), max(derlay_only_list_12)]
+    max_values = [max(sublist) if sublist else float('inf') for sublist in [derlay_only_list_1, derlay_only_list_4, derlay_only_list_8, derlay_only_list_12]]
 
     # Create bar positions
     x = numpy.arange(4)
@@ -243,49 +298,8 @@ def delay_graph_2(delay_lists):
 
     return None
 
-def delay_graph_3(delay_list):
-    return None
-
-# Accumulation of delays graph
-def lost_message_graph_2(unmatched_rows_list):
-    
-    unmatched_1 = []
-    unmatched_4 = []
-    unmatched_8 = []
-    unmatched_12 = []
-    
-    # Separate the data into four lists based on the message lenght
-    for message, time_start, time_finished, time_difference, answer, lat, lon, speed in unmatched_rows_list:
-        if len(message)/2 == 1:
-            unmatched_1.append(speed)
-        elif len(message)/2 == 4:
-            unmatched_4.append(speed)
-        elif len(message)/2 == 8:
-            unmatched_8.append(speed)
-        elif len(message)/2 == 12:
-            unmatched_12.append(speed)
-
-    # Create a cumulative distribution plot for each dataset
-    plt.hist(unmatched_1, bins=1000, density=True, histtype='step', color='blue', cumulative=True, alpha=0.5, label='Message lenght 1')
-    plt.hist(unmatched_4, bins=1000, density=True, histtype='step', color='green', cumulative=True, alpha=0.5, label='Message lenght 4')
-    plt.hist(unmatched_8, bins=1000, density=True, histtype='step', color='red',cumulative=True, alpha=0.5, label='Message lenght 8')
-    plt.hist(unmatched_12, bins=1000, density=True, histtype='step', color='black',cumulative=True, alpha=0.5, label='Message lenght 12')
-
-    # Add labels and title
-    plt.xlabel('Speed')
-    plt.ylabel('Cumulative Probability')
-    plt.title('Cumulative Distribution Plot')
-
-    # Add a legend
-    plt.legend()
-
-    # Show the plot
-    plt.grid()
-    plt.show()
-
-    return None
-
-def loacation_map(matching_pairs_list, unmatched_rows_list):
+# Location map html
+def location_map(matching_pairs_list, unmatched_rows_list):
     
 
     
@@ -322,20 +336,14 @@ def main():
     # Lost message percentage graph
     lost_message_graph(matching_pairs_list, unmatched_rows_list)
     
-    # Lost message (speed)
-    lost_message_graph_2(unmatched_rows_list)
-    
     # Delay graph
     delay_graph(delay_lists)
     
     # Delay graph 2 (min/average/max)
     delay_graph_2(delay_lists)
 
-    # Delay graph 3 (speed)
-    delay_graph_3(delay_lists)
-    
-    # map
-    loacation_map(matching_pairs_list, unmatched_rows_list)
+    # Map
+    location_map(matching_pairs_list, unmatched_rows_list)
     
     
 
